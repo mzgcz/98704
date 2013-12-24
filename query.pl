82ed3968197b937a3212896440105c1c6d5219a6
mzgcz <j.wenjiao@gmail.com>
2013-12-24 16:42:13 +0800

    添加多种格式输出

--- query.pl
+++ query.pl
@@ -4,37 +4,42 @@ use strict;
 use warnings;
 use utf8;
 use Mojo::JSON;
+use Mojo::Parameters;
 require LWP::UserAgent;
 
 binmode(STDOUT, ':encoding(utf8)');
 
-my $query_date = "2013-12-27";
-my $from_station = "NJH";
-my $to_station = "NCG";
-
-if (@ARGV > 0) {
-  $query_date = $ARGV[0];
-}
-
-if (@ARGV > 1) {
-  $from_station = $ARGV[1];
-}
-
-if (@ARGV > 2) {
-  $to_station = $ARGV[2];
-}
+my $query_date = $ARGV[0] || "2014-01-01";
+my $from_station = $ARGV[1] || "NJH";
+my $to_station = $ARGV[2] || "NCG";
+my $echo_type = $ARGV[3] || 1;
 
 my $ua = LWP::UserAgent->new();
 $ua->ssl_opts( verify_hostname => 0 );
 $ua->timeout(60);
 $ua->env_proxy;
 
-my $base_url = "https://kyfw.12306.cn/otn/lcxxcx/query?purpose_codes=ADULT";
-my $request_url = $base_url."&queryDate=".$query_date."&from_station=".$from_station."&to_station=".$to_station;
+my $query_base_url = "https://kyfw.12306.cn/otn/lcxxcx/query";
+my $query_params = Mojo::Parameters->new(
+                                         'purpose_codes' => 'ADULT',
+                                         'queryDate' => $query_date,
+                                         'from_station' => $from_station,
+                                         'to_station' => $to_station
+                                        );
+my $query_url = $query_base_url."?".$query_params;
 
-printf "%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\n", "车次", "出发", "抵达", "软卧", "硬卧", "硬座";
+if ($echo_type == 1) {
+  printf "%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\n",
+    "车次", "出发", "抵达", "软卧", "硬卧", "硬座";
+} elsif ($echo_type == 2) {
+  printf "%s\t\t%s\t\t%s\t\t%s\t\t%s\n",
+    "车次", "出发", "抵达", "一等", "二等";
+} else {
+  printf "%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\n",
+    "车次", "出发", "抵达", "软卧", "硬卧", "软座", "硬座", "一等", "二等";
+}
 
-my $response = $ua->get($request_url);
+my $response = $ua->get($query_url);
 if ($response->is_success) {
   my $json = Mojo::JSON->new;
   my $request_json = $json->decode($response->decoded_content);
@@ -42,19 +47,29 @@ if ($response->is_success) {
   foreach my $data (@$datas) {
     if ($data->{"canWebBuy"} eq "Y") {
       my $train_code = $data->{"station_train_code"}; # 车次
-      my $start_time = $data->{"start_time"};         # 出发时间
-      my $arrive_time = $data->{"arrive_time"};       # 抵达时间
+      my $start_time = $data->{"start_time"};         # 出发
+      my $arrive_time = $data->{"arrive_time"};       # 抵达
       my $rw_num = $data->{"rw_num"};                 # 软卧
       my $yw_num = $data->{"yw_num"};                 # 硬卧
-      my $gr_num = $data->{"gr_num"};                 # 高级软卧
+      my $gr_num = $data->{"gr_num"};                 # 高软
       my $rz_num = $data->{"rz_num"};                 # 软座
       my $yz_num = $data->{"yz_num"};                 # 硬座
-      my $swz_num = $data->{"swz_num"};               # 商务座
-      my $tz_num = $data->{"tz_num"};                 # 特等座
-      my $zy_num = $data->{"zy_num"};                 # 一等座
-      my $ze_num = $data->{"ze_num"};                 # 二等座
+      my $swz_num = $data->{"swz_num"};               # 商务
+      my $tz_num = $data->{"tz_num"};                 # 特等
+      my $zy_num = $data->{"zy_num"};                 # 一等
+      my $ze_num = $data->{"ze_num"};                 # 二等
       my $wz_num = $data->{"wz_num"};                 # 无座
-      printf "%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\n", $train_code, $start_time, $arrive_time, $rw_num, $yw_num, $yz_num;
+
+      if ($echo_type == 1) {
+        printf "%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\n",
+          $train_code, $start_time, $arrive_time, $rw_num, $yw_num, $yz_num;
+      } elsif ($echo_type == 2) {
+        printf "%s\t\t%s\t\t%s\t\t%s\t\t%s\n",
+          $train_code, $start_time, $arrive_time, $zy_num, $ze_num;
+      } else {
+        printf "%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\n",
+          $train_code, $start_time, $arrive_time, $rw_num, $yw_num, $rz_num, $yz_num, $zy_num, $ze_num;
+      }
     }
   }
 }
